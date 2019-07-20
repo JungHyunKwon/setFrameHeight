83084f94aa61d6d5ceea435385e36d367e4a4ae5
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:15:10 +0900

    published

--- js/setFrameHeight.js
+++ js/setFrameHeight.js
@@ -2,23 +2,19 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function() {
-		'use strict';
+(function() {
+	'use strict';
 
-		/**
-		 * @name setFrameHeight
-		 * @since 2017-12-06
-		 * @param {element} element
-		 */
-		window.setFrameHeight = function(element) {
-			try {
-				element.height = element.contentWindow.document.documentElement.scrollHeight;
-			}catch(e) {
-				//throw e;
-			}
-		};
-	})();
-}catch(e) {
-	console.error(e);
-}
+	/**
+	 * @name setFrameHeight
+	 * @since 2017-12-06
+	 * @param {object} element
+	 */
+	window.setFrameHeight = function(element) {
+		try {
+			element.height = element.contentWindow.document.documentElement.scrollHeight;
+		}catch(e) {
+			//console.error(e);
+		}
+	};
+})();
